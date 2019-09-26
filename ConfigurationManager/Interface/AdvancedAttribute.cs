eab5ab988ad154e764a26016a34a070dce05d23f
ManlyMarco <dummyaccx@gmail.com>
2019-09-26 21:08:44 +0200

    Move config manager from BepisPlugins

---
+++ ConfigurationManager/Interface/AdvancedAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BepInEx
+{
+    [Obsolete("Pass string \"Advanced\" as a tag instead")]
+    public class AdvancedAttribute : Attribute
+    {
+        public AdvancedAttribute(bool isAdvanced) => IsAdvanced = isAdvanced;
+
+        public bool IsAdvanced { get; }
+    }
+}
---
+++ ConfigurationManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("ConfigurationManager")]
+[assembly: AssemblyTitle("BepisPlugins.ConfigurationManager")]
