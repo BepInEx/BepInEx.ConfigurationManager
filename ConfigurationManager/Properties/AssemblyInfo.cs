f9ad2b40505d515fb497f0f166d1a70283034321
ManlyMarco <dummyaccx@gmail.com>
2019-09-29 21:38:46 +0200

    Add missing assembly metadata

--- ConfigurationManager/Properties/AssemblyInfo.cs
+++ ConfigurationManager/Properties/AssemblyInfo.cs
@@ -1,4 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("ConfigurationManager")]
-[assembly: AssemblyTitle("BepisPlugins.ConfigurationManager")]
+[assembly: AssemblyProduct("BepInEx.ConfigurationManager")]
+[assembly: AssemblyTitle("BepInEx.ConfigurationManager")]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/BepInEx/BepInEx.ConfigurationManager")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(ConfigurationManager.ConfigurationManager.Version)]
