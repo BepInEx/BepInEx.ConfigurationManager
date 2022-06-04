a662c830c28bbc2eadf7e3e3b8790a561868098c
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-06-05 00:27:09 +0200

    Version up to 17.0

--- ConfigurationManager/ConfigurationManager.cs
+++ ConfigurationManager/ConfigurationManager.cs
@@ -31,7 +31,7 @@ namespace ConfigurationManager
         /// <summary>
         /// Version constant
         /// </summary>
-        public const string Version = "16.4";
+        public const string Version = "17.0";
 
         internal static new ManualLogSource Logger;
         private static SettingFieldDrawer _fieldDrawer;
