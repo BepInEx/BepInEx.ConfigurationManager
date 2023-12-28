0b502ad67898ffb11d3f28020774e3fcf676af32
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-12-28 23:44:17 +0100

    Version up to 18.2

--- ConfigurationManager.Shared/ConfigurationManager.cs
+++ ConfigurationManager.Shared/ConfigurationManager.cs
@@ -38,7 +38,7 @@ namespace ConfigurationManager
         /// <summary>
         /// Version constant
         /// </summary>
-        public const string Version = "18.1";
+        public const string Version = "18.2";
 
         internal static ManualLogSource Logger;
         private static SettingFieldDrawer _fieldDrawer;
